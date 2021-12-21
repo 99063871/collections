323adb9b2d1b8e171ed19ca1ba211effc7337d44
99063871 <99063871@mydavinci.com>
2021-12-21 10:17:48 +0100

    spelprogramma 1 gemaakt

--- Spelprogramma/Spelprogramma.py
+++ Spelprogramma/Spelprogramma.py
@@ -1,12 +1,15 @@
 import random
 
 spelList = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
+gameList = []
 
-def spelProgramma():
+def spelProgramma(list):
     totalAmount = random.randint(3,10)
     for a in range(totalAmount):
         amount = len(spelList) - 1
         games = random.randint(0, amount)
-        print(spelList[games])
+        game = spelList[games]
+        gameList.append(game)
+    return gameList
 
-spelProgramma()
+print(spelProgramma(gameList))
