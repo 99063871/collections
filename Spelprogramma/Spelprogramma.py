2efbb46c7f8b6a0d4a43d68d51e213ff6db0545a
99063871 <99063871@mydavinci.com>
2021-12-21 10:20:29 +0100

    feature van minimum toegevoegd

--- Spelprogramma/Spelprogramma.py
+++ Spelprogramma/Spelprogramma.py
@@ -2,9 +2,8 @@ import random
 
 spelList = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
 gameList = []
-
-def spelProgramma(list):
-    totalAmount = random.randint(3,10)
+def spelProgramma(list, minimum):
+    totalAmount = random.randint(minimum,10)
     for a in range(totalAmount):
         amount = len(spelList) - 1
         games = random.randint(0, amount)
@@ -12,4 +11,4 @@ def spelProgramma(list):
         gameList.append(game)
     return gameList
 
-print(spelProgramma(gameList))
+print(spelProgramma(gameList, 3))
